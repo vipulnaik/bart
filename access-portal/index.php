3abf6dde59506396c4cc82320ebcf470eb2cba68
Vipul Naik <vipulnaik1@gmail.com>
2017-05-09 05:44:39 +0000

    Added some new files

--- access-portal/index.php
+++ access-portal/index.php
@@ -3,6 +3,7 @@ print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><hea
 print '<title>BART: Historical ridership, route, and price data</title>';
 # include_once('analytics.inc');
 # include_once('backend/stringFunctions.inc');
+include_once("backend/globalVariables/passwordFile.inc");
 print '<link href="style.css" rel="stylesheet" type="text/css" />'."\n";
 print '<script type="text/javascript" src="./jquery-3.1.1.min.js"></script>'."\n";
 print '<script type="text/javascript" src="./jquery.tablesorter.js"></script>'."\n";
@@ -13,5 +14,20 @@ print '<script>$(document).ready(function()
     }
 ); </script>'."\n";
 include_once('preamble.inc');
+$stationVariableName = "entry_station";
+print '<form method="post" action="index.php">';
+print '<table>';
+print '<tr>';
+print '<td>Entry station (station 1)</td>';
+include("backend/stationDropdown.inc");
+print '</tr>';
+print '<tr>';
+print '<td>Exit station (station 2)</td>';
+$stationVariableName = "exit_station";
+include("backend/stationDropdown.inc");
+print '<tr><td><input type="submit" value="Submit"></td>';
+print '</tr>';
+print '</table>';
+print '</form>';
 print '</body>';
 ?>

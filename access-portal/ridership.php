117b4b839e6b89b52a92a362443c71284d0fc3ee
Vipul Naik <vipulnaik1@gmail.com>
2017-05-21 01:30:21 +0000

    Now added ridership data

--- access-portal/ridership.php
+++ access-portal/ridership.php
@@ -1,8 +1,15 @@
 <?php
 print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
-print '<title>BART: Historical ridership, route, and price data</title>';
-# include_once('analytics.inc');
-# include_once('backend/stringFunctions.inc');
+$entryStation = '12';
+if (!empty($_REQUEST['entryStation'])) {
+  $entryStation = $_REQUEST['entryStation'];
+}
+$exitStation = '12';
+if (!empty($_REQUEST['exitStation'])) {
+  $exitStation = $_REQUEST['exitStation'];
+}
+
+print "<title>BART Ridership: $entryStation to $exitStation</title>";
 include_once("backend/globalVariables/passwordFile.inc");
 include_once("backend/globalVariables/globalVariables.inc");
 print '<link href="style.css" rel="stylesheet" type="text/css" />'."\n";
@@ -12,6 +19,7 @@ print '</head>';
 print '<body>';
 print '<script>$(document).ready(function()
     {
+        $("#ridershipData").tablesorter();
     }
 ); </script>'."\n";
 include_once('preamble.inc');
@@ -30,5 +38,8 @@ print '<tr><td><input type="submit" value="Submit"></td>';
 print '</tr>';
 print '</table>';
 print '</form>';
+include_once("backend/ridershipQueries.inc");
+include_once("backend/displayFunctions.inc");
+printRidership($entryStation, $exitStation);
 print '</body>';
 ?>

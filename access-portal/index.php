7cb030d7e5c963d16ca0a8fa732a443498630e1b
Vipul Naik <vipulnaik1@gmail.com>
2020-07-17 03:20:22 +0000

    Remove unused JS file

--- access-portal/index.php
+++ access-portal/index.php
@@ -5,8 +5,9 @@ include_once('analytics.inc');
 include_once("backend/globalVariables/passwordFile.inc");
 include_once("backend/globalVariables/globalVariables.inc");
 print '<link href="style.css" rel="stylesheet" type="text/css" />'."\n";
-print '<script type="text/javascript" src="./jquery-3.1.1.min.js"></script>'."\n";
-print '<script type="text/javascript" src="./jquery.tablesorter.js"></script>'."\n";
+# The resources below are currently not needed on the home page
+# print '<script type="text/javascript" src="./jquery-3.1.1.min.js"></script>'."\n";
+# print '<script type="text/javascript" src="./jquery.tablesorter.js"></script>'."\n";
 print '</head>';
 print '<body>';
 print '<script>$(document).ready(function()

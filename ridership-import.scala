b644f23b38a1e88821c5cd41659b98133e9bb1f5
Vipul Naik <vipulnaik1@gmail.com>
2021-07-03 09:29:13 -0700

    Add ridership for months of March to June 2021

--- ridership-import.scala
+++ ridership-import.scala
@@ -4,7 +4,7 @@ import java.util.Date
 val sb = new StringBuilder
 val forbiddenStationCodes = Nil // Seq("ML", "BE")
 for (year <- 2001 to 2021) {
-  for (month <- 1 to {if (year == 2021) 2 else 12}) {
+  for (month <- 1 to {if (year == 2021) 6 else 12}) {
     val monthfull = s"$year-" + "%02d".format(month)
     for (dayType <- Seq("weekday", "saturday", "sunday")) try {
       val path = s"/Users/vipulnaik/git/bart/ridership/$monthfull/$dayType.csv"

bee2e873c250f37ec917023667b13ce5cd74b68e
Vipul Naik <vipulnaik1@gmail.com>
2021-07-18 04:55:56 +0000

    Lighthouse improvements

--- access-portal/index.php
+++ access-portal/index.php
@@ -1,5 +1,5 @@
 <?php
-print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
+print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html lang="en-US"><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8">';
 print '<title>BART: Historical ridership, route, and price data</title>';
 include_once('analytics.inc');
 include_once("backend/globalVariables/passwordFile.inc");
@@ -10,10 +10,10 @@ print '<link href="style.css" rel="stylesheet" type="text/css" />'."\n";
 # print '<script type="text/javascript" src="./jquery.tablesorter.js"></script>'."\n";
 print '</head>';
 print '<body>';
-print '<script>$(document).ready(function()
-    {
-    }
-); </script>'."\n";
+# print '<script>$(document).ready(function()
+#     {
+#     }
+# ); </script>'."\n";
 include_once('preamble.inc');
 
 # Single station both sides
--- access-portal/ridership.php
+++ access-portal/ridership.php
@@ -1,5 +1,5 @@
 <?php
-print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
+print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html lang="en-US"><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
 $entryStation = '12';
 if (!empty($_REQUEST['entryStation'])) {
   $entryStation = $_REQUEST['entryStation'];

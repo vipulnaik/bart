8bf565f416ac7ba36294994fc27ff9e572b813f0
Vipul Naik <vipulnaik1@gmail.com>
2021-07-18 15:54:12 +0000

    Followup to bee2e873c250f37ec917023667b13ce5cd74b68e

--- access-portal/ridership-multiple.php
+++ access-portal/ridership-multiple.php
@@ -1,5 +1,5 @@
 <?php
-print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
+print '<!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN"><html lang="en-US"><head><meta http-equiv="Content-Type" content="text/html;charset=utf-8" >';
 $entryStationsList = array();
 if (!empty($_REQUEST['entryStationsList'])) {
   $entryStationsList = $_REQUEST['entryStationsList'];
